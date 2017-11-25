f9610f72ce4ef4834ef7f12960216eda0d4ed5f4
Causality <avramihammer@gmail.com>
2017-11-25 14:50:02 +1100

    trying stuff

--- pcode 9-1000py Start BB backend.py
+++ pcode 9-1000py Start BB backend.py
@@ -3,9 +3,15 @@ from flask import Flask
 app = Flask(__name__)
 
 
+@app.route("/")
+def index():
+    return 'success', 200
+
+
 @app.route('/address/search')
 def search():
     return 'success', 200
+
 
 if __name__ == '__main__':
     app.run()
