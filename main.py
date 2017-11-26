8f36959955d84adeadd55307e5f1052394965a84
Causality <avramihammer@gmail.com>
2017-11-26 12:23:32 +1100

    Fixed Main

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
 import json
 
 import requests
-import startBB
 from flask import Flask, request
 from requests import ConnectionError
 from requests.auth import HTTPBasicAuth
 
-# try:
-#     from config import authorization, telstra_location_url, telstra_details_url
-# except ImportError:
-import os
+import startBB
+from config import authorization, telstra_location_url, telstra_details_url
 
-authorization = {
-    "username": os.getenv("username"),
-    "password": os.getenv("password"),
-}
-telstra_location_url = os.getenv("telstra_location_url")
-telstra_details_url = os.getenv("telstra_details_url")
+# import os
+#
+# authorization = {
+#     "username": os.getenv("username"),
+#     "password": os.getenv("password"),
+# }
+# telstra_location_url = os.getenv("telstra_location_url")
+# telstra_details_url = os.getenv("telstra_details_url")
 app = Flask(__name__)
 
 
@@ -27,7 +26,7 @@ def index():
 
 @app.route('/address/get', methods=['POST'])
 def get():
-    client_request = json.loads(request.data)
+    client_request = json.loads(request.data, encoding='utf8')
     success = do_telstra_service_lookup(client_request["locationId"],
                                         client_request)
     return success
@@ -37,10 +36,13 @@ def get():
 def search():
     try:
         client_request = json.loads(request.data)
-        result = do_telstra_location_lookup(client_request)
+        telstra_address = maps_to_telstra_address(client_request["address"])
+        if not telstra_address:
+            return 'Address is not valid', 400
+        result = do_telstra_location_lookup(telstra_address)
         if result is False:
-            return 'No Line Available', 437
-        if result is None:
+            return 'No Line Exists', 404
+        if result is None or len(result) == 0:
             return 'Bad Request', 400
         if len(result) > 1:
             print("Warning: Address filter failed")
@@ -56,6 +58,7 @@ def search():
 
 def do_telstra_location_lookup(args):
     data = json.dumps(args)
+    print(data)
     header = {
         "Content-Type": "application/json",
     }
@@ -98,6 +101,35 @@ def do_telstra_service_lookup(locationId, args):
     configured_data = startBB.main(response)
     return configured_data
 
+
+def maps_to_telstra_address(maps_address):
+    telstra = {"address": {
+        "unitNumber": "",
+        "streetNumber": "",
+        "streetName": "",
+        "streetType": "",
+        "suburb": "",
+        "state": "",
+        "postcode": ""
+    }}
+    for field in maps_address["address_components"]:
+        type = field["types"]
+        value = field["long_name"].encode('utf-8')
+        if "subpremise" in type:
+            telstra["address"]["unitNumber"] = value
+        if "street_number" in type:
+            telstra["address"]["streetNumber"] = value
+        if "route" in type:
+            telstra["address"]["streetName"], telstra["address"][
+                "streetType"] = value.rsplit(" ", 2)
+        if "locality" in type:
+            telstra["address"]["suburb"] = value
+        if "administrative_area_level_1" in type:
+            telstra["address"]["state"] = field["short_name"].encode('utf-8')
+        if "postal_code" in type:
+            telstra["address"]["postcode"] = value
+
+    return telstra
+
 if __name__ == '__main__':
     app.run()
-
